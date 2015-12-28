fd78863b0ac3767c54240db68b78c45db347341d
Jan Andersson <jan.andersson@gmail.com>
2015-12-28 10:59:23 +0100

    Add initial Translation class

---
+++ Vortaro/Translation.swift
@@ -0,0 +1,23 @@
+//
+//  Translation.swift
+//  Vortaro
+//
+//  Created by Jan Andersson on 2015-12-28.
+//  Copyright © 2015 Visuell Data. All rights reserved.
+//
+
+import Foundation
+
+class Translation {
+    var eo: String
+    var en: String
+
+    init(eo: String, en: String) {
+        self.eo = eo
+        self.en = en
+    }
+
+    var description: String {
+        return "\(eo): \(en)"
+    }
+}
