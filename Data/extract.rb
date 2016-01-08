c587ca211185b51ad0e18433878db92fb6109328
Jan Andersson <jan.andersson@gmail.com>
2016-01-08 12:04:34 +0100

    Sort case insensitive

--- Data/extract.rb
+++ Data/extract.rb
@@ -21,7 +21,13 @@ for line in lines
   end
 end
 
+sorter = lambda {|a, b| SortAlphabetical.normalize(a.downcase) <=> SortAlphabetical.normalize(b.downcase) }
+
+puts "Creating eo_to_ens.json..."
 File.open("../Vortaro/eo_to_ens.json", "w") { |f| f.write(eo_to_ens.to_json) }
+puts "Creating en_to_eos.json..."
 File.open("../Vortaro/en_to_eos.json", "w") { |f| f.write(en_to_eos.to_json) }
-File.open("../Vortaro/eos.txt", "w") { |f| f.write(eo_to_ens.keys.sort_alphabetical.join("\n")) }
-File.open("../Vortaro/ens.txt", "w") { |f| f.write(en_to_eos.keys.sort_alphabetical.join("\n")) }
+puts "Creating eos.txt..."
+File.open("../Vortaro/eos.txt", "w") { |f| f.write(eo_to_ens.keys.sort(&sorter).join("\n")) }
+puts "Creating ens.txt..."
+File.open("../Vortaro/ens.txt", "w") { |f| f.write(en_to_eos.keys.sort(&sorter).join("\n")) }
